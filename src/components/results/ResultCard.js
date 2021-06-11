086d6dc3badf8762589c55d953851ce9cad65de7
ghostovsky2 <szymonsoborowski@gmail.com>
2021-06-11 19:32:07 +0200

    some stuff added

--- src/components/results/ResultCard.js
+++ src/components/results/ResultCard.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState} from 'react'
+
 
 
 export default function ResultCard() {
+
+    const link = window.location.href.split('/')[4];
+    const [data, setData] = useState({})
+
+    useEffect(()=>{
+        axios.get('https://api.cepik.gov.pl/pojazdy/'+link)
+            .then(res => {
+                setData(res.data.data.attributes);
+            })
+    }, [])
     return (
         <div>
-            
+            {console.log(data)}
+            <h1>{data.marka} {data.model}</h1>
+            <ul>
+                <li>dopuszczalna masa calkowita: {data['dopuszczalna-masa-calkowita']}</li>
+                <li>Czy ma hak: {data.hak ? "tak" : "nie"}</li>
+                <li>podrodzaj pojazdu: {data['podrodzaj-pojazdu']}</li>
+                <li>pojemność skokowa silnika: {data['pojemnosc-skokowa-silnika']}</li>
+                <li>rodzaj paliwa: {data['rodzaj-paliwa']}</li>
+                <li>rodzaj pojazdu: {data['rodzaj-pojazdu']}</li>
+                <li>sposób produkcji: {data['sposob-produkcji']}</li>
+            </ul>
         </div>
     )
 }
--- src/components/results/Results.js
+++ src/components/results/Results.js
@@ -14,56 +14,47 @@ export default function Results({answers}) {
     const [apiCars, setApiCars] = useState([]);
     const [loader, setLoader] = useState(false);
 
-    // useEffect(() => {
-        // const date = new Date();
-        // const year = (date.getFullYear() - 2).toString();
-        // const month = date.getMonth() + 1 < 10 ? "0" + (date.getMonth() + 1).toString() : (date.getMonth() + 1).toString();
-        // const day = date.getDate() < 10 ? "0" + date.getDate().toString() : date.getDate().toString();
-        // axios.get(`https://api.cepik.gov.pl/pojazdy?wojewodztwo=XX&data-od=${year + month + day}&limit=5&filter[rodzaj-pojazdu]=${answers[0]}&filter[marka]=${answers[1]}&filter[rok-produkcji]=${answers[2]}&filter[rodzaj-paliwa]=${answers[3]}&filter[liczba-miejsc-ogolem]=${answers[4]}`)
-        //     .then(res => {
-        //         setApiCars(apiCars.concat(res.data.data));
-        //     })
-        //     .catch(() => console.log('coś poszło nie tak'))
-        // for (let i = 2; i <= 32; i += 2) {
-        //     console.log(answers);
-        //     axios.get(`https://api.cepik.gov.pl/pojazdy?wojewodztwo=${i < 10 ? "0" : ""}${i}&data-od=${year + month + day}&limit=5&filter[rodzaj-pojazdu]=${answers[0]}&filter[marka]=${answers[1]}&filter[rok-produkcji]=${answers[2]}&filter[rodzaj-paliwa]=${answers[3]}&filter[liczba-miejsc-ogolem]=${answers[4]}`)
-        //         .then(res => {
-        //             setApiCars(apiCars.concat(res.data.data));
-        //             console.log(`https://api.cepik.gov.pl/pojazdy?wojewodztwo=${i < 10 ? "0" : ""}${i}&data-od=${year + month + day}&limit=5&filter[rodzaj-pojazdu]=${answers[0]}&filter[marka]=${answers[1]}&filter[rok-produkcji]=${answers[2]}&filter[rodzaj-paliwa]=${answers[3]}&filter[liczba-miejsc-ogolem]=${answers[4]}`);
-        //         })
-        //         .catch(() => console.log('coś poszło nie tak'))
-    //     }
-    // })
+    const date = new Date();
+    const year = (date.getFullYear() - 2).toString();
+    const month = date.getMonth() + 1 < 10 ? "0" + (date.getMonth() + 1).toString() : (date.getMonth() + 1).toString();
+    const day = date.getDate() < 10 ? "0" + date.getDate().toString() : date.getDate().toString();
+    const days = answers[2].split('-');
+
+    useEffect(() => {
+        for (let i = Number.parseInt(days[0]); i <= Number.parseInt(days[1]); i++)
+            axios.get(`https://api.cepik.gov.pl/pojazdy?wojewodztwo=02&data-od=${year + month + day}&limit=2&filter[rodzaj-pojazdu]=${answers[0]}&filter[marka]=${answers[1]}&filter[rok-produkcji]=${i}&filter[rodzaj-paliwa]=${answers[3]}&filter[liczba-miejsc-ogolem]=${answers[4]}`)
+                .then(res => {
+                    setApiCars(apiCars.concat(res.data.data));
+                })
+                .catch((err) => console.log(err))
+    }, [])
 
     return (
         <div className="result-container">
             {
-                apiCars.map((element, index) => {
-                    if(index<10){
+                apiCars.map((element, index)=>{
+                    
                         return <Card key={index} className='result-card'>
-                            <CardActionArea>
-                                <CardMedia
-                                    className='result-image'
-                                    image="../../../img/slider1.jpg"
-                                />
-                                <CardContent>
-                                    <Typography gutterBottom variant="h5" component="h2">
-                                        Lizard
-                                    </Typography>
-                                    <Typography variant="body2" color="textSecondary" component="p">
-                                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                                        across all continents except Antarctica
-                                    </Typography>
-                                </CardContent>
-                            </CardActionArea>
-                            <CardActions>
-                                <Button size="small">Learn More</Button>
-                            </CardActions>
-                        </Card>
-                    }
+                                    <CardMedia
+                                        className='result-image'
+                                        image="../../../img/slider1.jpg"
+                                    />
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="h2">
+                                        {element.attributes.marka} {element.attributes.model}
+                                        </Typography>
+                                        <Typography variant="body2" color="textSecondary" component="p">
+                                            rok produkcji: {element.attributes['rok-produkcji']}
+                                        </Typography>
+                                    </CardContent>
+                            
+                                <CardActions>
+                                    <Link to={"/carCard/"+element.id}><Button size="small">Zobacz więcej</Button></Link>
+                                </CardActions>
+                            </Card>
                 })
             }
-            <Link to="/carCard">Zobacz więcej</Link>
+            
         </div>
     )
 }
