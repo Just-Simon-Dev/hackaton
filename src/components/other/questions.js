029af8d560ff91ebe4c738911e050e7267eb9a75
Zyziolez <zuzannaoficjalny@gmail.com>
2021-06-11 17:59:14 +0200

    FORMULARZ v4

--- src/components/other/questions.js
+++ src/components/other/questions.js
@@ -9,7 +9,7 @@ class Question {
 export const questions = [
     new Question( '1. Jakiego rodzaju pojazdu szukasz?', [ 'samochód osobowy', 'samochód ciężarowy' ] ),
     new Question( '2. Jaką markę pojazdu preferujesz?', [ 'Audi', 'Opel', 'Ford', 'Volkswagen', 'Renault', 'Kia' ] ),
-    new Question('3. W jakim przedziale czasowym chcesz aby auto było wyprodukowane:', ['2016-2021', '2010-2015', '2004-2009', '1998-2003']),
+    new Question('3. W jakim przedziale czasowym chcesz aby auto było wyprodukowane:', ['2016-2021', '2010-2015', '2004-2009', 'Fabryczny']),
     new Question( '4. Jaki rodzaj paliwa wolisz?', [ 'Olej napędowy', 'Benzyna', 'Energia elektryczna', 'Gaz' ] ),
     new Question('5. Ile miejsc ma posiadać pojazd?', [ 2, 5, 7 ])
 ]
