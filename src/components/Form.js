ef86065e690e9c793923d831a6696dd4fefc3dcf
ghostovsky2 <szymonsoborowski@gmail.com>
2021-06-11 17:38:41 +0200

    add some stuff

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Results from './results/Results'
 
 export default function Form() {
     return (
         <div>
-            
+            <Results/>
         </div>
     )
 }
