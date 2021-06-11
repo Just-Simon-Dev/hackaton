d356b3ff8c13b16312cbaf41030d311174d1f2ef
Zyziolez <zuzannaoficjalny@gmail.com>
2021-06-11 15:11:45 +0200

    styl do kart

--- src/components/Safety.js
+++ src/components/Safety.js
@@ -8,14 +8,6 @@ import Typography from '@material-ui/core/Typography'
 
 export default function Safety() {
 
-    const useStyles = makeStyles({
-        root: {
-            width: "33%",
-            height: "211px",
-            display: "inline-block",
-        },
-    });
-
     const context = [
         { name: "Artykuł 1", link: "http://skoda-safety.wp.pl/artykul/czy-male-i-nowoczesne-auta-sa-bezpieczniejsze-niz-wieksze-i-starsze", context: "Zastanawiasz się nad pierwszym autem i chciałbyś, żeby było bezpieczne? A może chcesz dowiedzieć się o nowinkach motoryzacyjnych. To jest artykuł, w którym są poruszane najważniejsze kwestie bezpieczeństwa auta od reflektorów po nowoczesne systemy wspomagania kierowcy."},
         { name: "Artykuł 2", link: "https://forsal.pl/artykuly/1430650,impact-czy-samochody-elektryczne-sa-bezpieczniejsze-od-samochodow-spalinowych.html", context: "Chciałbyś zaczerpnąć wiedzy na  temat bezpieczeństwa samochodów elektrycznych? Chciałbyś zobaczyć twarde statystyki? O to artykuł doskonały dla ciebie! Znajdziesz tutaj informacje na temat pożarów samochodów elektrycznych oraz crash testach." },
@@ -24,12 +16,11 @@ export default function Safety() {
         { name: "Artykuł 5", link: "https://www.autoexpert.pl/artykuly/wplyw-nowych-technologii-na-bezpieczenstwo-jazdy-samochodow", context: "Chciałbyś wiedzieć o nowinkach technologicznych w branży motoryzacyjnej? A może ciekawisz się jak udoskonalić swoją jazdę? Jeśli tak, to jak najszybciej musisz przeczytać właśnie ten artykuł!" },
         { name: "Artykuł 6", link: "https://mojafirma.infor.pl/moto/eksploatacja-auta/porady-uzytkowe/697553,Co-w-samochodzie-odpowiada-za-bezpieczenstwo-pasazerow.html", context: "W tym artykule jest opisane bezpieczeństwo w pigułce. Chciałbyś poznać twarde fakty o systemach wspomagających kierowcę albo co chroni kierowcę i pasażerów podczas jazdy? To jest ten artykuł, który powinieneś przeczytać!" },
     ]
-    const classes = useStyles();
     return (
         <div className="safety-container">
             {
                 context.map((element, i) => {
-                    return <Card className={classes.root} key={i}>
+                    return <Card className='karty' key={i}  >
                         <CardContent>
                             <Typography variant="h5" component="h2">
                                 {element.name}
