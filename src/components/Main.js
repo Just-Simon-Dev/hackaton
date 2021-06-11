c74724474099b3d1adf2740cf1082c1d43b9779f
Zyziolez <zuzannaoficjalny@gmail.com>
2021-06-11 19:29:43 +0200

    strona glowna

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import {images} from './other/images'
+import { Fade } from 'react-slideshow-image';
+import { Link } from 'react-router-dom';
+import { Button }  from '@material-ui/core'
 
 export default function Main() {
     return (
-        <div>
-            
-        </div>
+        <div id='mainer' >
+            <div id='slider' >
+            <Fade arrows={false} duration={5000} pauseOnHover={false} >
+                    { images.map( (zdj, i) => 
+                        <div key={i} className='slide' >
+                            <img src={zdj} key={i}   />
+                            <div className='text-rog'  >
+                                <h1>RUI - miasto przyszłości</h1>
+                                <p> Zrób quiz i zobacz wymarzone auto </p>
+                                <Link to='formularz' ><Button> Zobacz </Button></Link>
+                            </div>
+                        </div>
+                    )}
+                </Fade>
+            </div>
+            </div>
     )
 }
---
+++ src/components/other/images.js
@@ -0,0 +1,7 @@
+import sl1 from './../../img/slider1.jpg';
+import sl2 from './../../img/slider2.jpg';
+import sl3 from './../../img/slider3.jpg';
+import sl4 from './../../img/slider4.jpg';
+import sl5 from './../../img/slider5.jpg';
+
+export  const images = [ sl1, sl2, sl3, sl4, sl5 ]
